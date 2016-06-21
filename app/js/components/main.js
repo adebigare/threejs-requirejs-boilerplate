799cfe596a34fca34b4cc6c3a9532b558b0f3691
Adrienne Debigare <adebigare@gmail.com>
2016-06-21 00:25:20 -0400

    added better structure and commenting

--- app/js/components/main.js
+++ app/js/components/main.js
@@ -1,7 +1,7 @@
 require(['tween','three', 'VizView'], function(TWEEN,THREE, VizView) {
 
     (function(){
-        var vizView = new VizView(); 
+        var viz = new VizView(); 
     })();
     
 });
